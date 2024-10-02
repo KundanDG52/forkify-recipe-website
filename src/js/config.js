632fe183944390d3380e428b35013b138597d83d
Kundan Gahalot <gahalotkundan@gmail.com>
2024-10-02 10:42:49 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const API_KEY = 'c1dd1d05-af2f-456c-b6c6-c36468766937';
+export const MODAL_CLOSE_SEC = 2.5;
